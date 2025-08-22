8da491e6c5de1632d458d5b82d78ce42d2371d0d
Pedro <pedrobneto3@gmail.com>
2025-08-22 15:23:03 -0300

    Fixing update

--- src/common/update.lua
+++ src/common/update.lua
@@ -1,8 +1,3 @@
-
-local githubRepoUrl = "https://https://github.com/your-username/cc-manager"
-
-local cloneTargetDir = "cc-manager"
-
 print("Removing old project files...")
 local function deleteDir(path)
     if fs.exists(path) then
@@ -17,14 +12,6 @@ local function deleteDir(path)
     end
 end
 
-deleteDir(cloneTargetDir)
+deleteDir("manager")
 
-print("Updating files from GitHub repository '" .. githubRepoUrl .. "' into '" .. cloneTargetDir .. "' using Pastebin script...")
-local success, output = shell.run("pastebin", "run", "MViDbkcX", githubRepoUrl, cloneTargetDir)
-
-if not success then
-    error("GitHub cloning failed: " .. tostring(output))
-end
-
-print("Update complete. Rebooting system...")
-os.reboot()
+shell.run("pastebin", "run", "QCqV74ik")
